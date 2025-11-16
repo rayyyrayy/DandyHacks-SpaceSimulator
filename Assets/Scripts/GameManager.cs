d2ea22007c05612dd87d8e85da792fde4f35733d
rayyyrayy <raymondaw02@gmail.com>
2025-11-15 23:00:30 -0500

    Added basic dialogue

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,25 +8,39 @@ public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI introCaption;
     public AudioSource introMessage;
+    private bool missionMessageStarted=false;
+    public AudioSource missionMessage;
+    public TextMeshProUGUI missionMessageCaption;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(StartGame());
+        StartCoroutine(MissionMessage());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    IEnumerator startGame()
+    IEnumerator StartGame()
     {
         yield return new WaitForSeconds(2);
         introMessage.Play();
         introCaption.gameObject.SetActive(true);
         yield return new WaitForSeconds(7);
         introCaption.gameObject.SetActive(false);
+        missionMessageStarted=true;
+    }
+
+    IEnumerator MissionMessage()
+    {
+        while (missionMessageStarted == false)
+        {
+            yield return null; 
+        }
+        yield return new WaitForSeconds(1);
+        missionMessage.Play();
+        missionMessageCaption.gameObject.SetActive(true);
+        yield return new WaitForSeconds(7);
+        missionMessageCaption.gameObject.SetActive(false);
+
     }
 }
